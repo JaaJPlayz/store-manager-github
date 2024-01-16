307080cf753c00535ecf76bf3c68c8c0a10c015e
JaaJPlayz <hiagoaugustofcunha@gmail.com>
2024-01-16 11:10:19 -0300

    Making the first routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,4 +7,12 @@ app.get('/', (_request, response) => {
   response.json({ status: 'Store Manager UP!' });
 });
 
+app.get('/products', (_request, response) => {
+  response.status(200).send('rota para devolver produtos');
+});
+
+app.get('/products/:id', (_request, response) => {
+  response.status(200).send('rota para pegar um produto pelo id');
+});
+
 module.exports = app;
